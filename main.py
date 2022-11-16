8ff934af6fae9d712ee3e03760c03dfbcbcb28c8
Noah Da Silva <noah@noahdasilva.com>
2022-11-16 14:25:45 -0500

    Added theoretical value comparision.

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ from sys import exit as sys_exit
 
 import network_simulation as ns
 import analytics as a
+import theoretical as th
 
 
 # Generating a list of random numbers for random values needed in simulation.
@@ -39,75 +40,96 @@ def exp_rand_num_gen(seed=1, rate=1, size=1):
 
 
 def main():
-    arg_err_msg = "Please specify the value of λ (arrival rate) and μ (service rate) with syntax 'lambda=λ' and 'mu=μ'. (i.e. 'python3 -u main.py lambda=1 mu=3')"
+    arg_err_msg = "Please specify the value of λ (arrival rate), μ (service rate), and k (parallel servers) with syntax 'lambda=λ', 'mu=μ', and 'k=k'. (i.e. 'python3 -u main.py lambda=1,2,3 mu=3 k=2')"
     # Check for needed arguments when the file is executed.
-    if len(argv) != 3:
-        sys_exit(arg_err_msg)
-
-    # Verify if the first argument is either our lambda or mu, then verify if the last argument is the other.
-    # If there is no match, or any formatting errors, end the program and inform the user.
     try:
-        match argv[1].split('=')[0]:
-            case 'lambda':
-                LAMBDA = list(map(float, argv[1].split('=')[1].split(',')))
-                match argv[2].split('=')[0]:
-                    case 'mu':
-                        MU = float(argv[2].split('=')[1])
-                    case _:
-                        sys_exit(arg_err_msg)
-            case 'mu':
-                MU = float(argv[1].split('=')[1])
-                match argv[2].split('=')[0]:
-                    case 'lambda':
-                        LAMBDA = list(map(float, argv[2].split('=')[1].split(',')))
-                    case _:
-                        sys_exit(arg_err_msg)
-            case _:
+        args_dict= {}
+        # Go through each given argument, start at second argument (first is the filename).
+        for arg in argv[1:]:
+            # Split the argument as name and value.
+            arg_split = arg.split('=')
+            # Check for the values' name, if there is a match, set the value.
+            if arg_split[0] == 'lambda':
+                LAMBDA = list(map(float, arg_split[1].split(',')))
+            elif arg_split[0] == 'mu':
+                MU = float(arg_split[1])
+            elif arg_split[0] == 'k':
+                k = int(arg_split[1])
+            # If argument does not match wanted names, inform the user of issue.
+            else:
                 sys_exit(arg_err_msg)
+    # Try will fail if user did not input proper argument values (float vs int vs char).
     except:
         sys_exit(arg_err_msg)
 
-    avg_n_packets = []
-    avg_queue_delay = []
-    avg_waiting_time = []
-    netsim_data = a.NetworkSimulationAnalytics
+    simulation_avg_n_packets = []
+    simulation_avg_queue_delay = []
+    simulation_avg_waiting_time = []
+
+    theoretical_avg_n_packets = []
+    theoretical_avg_queue_delay = []
+    theoretical_avg_waiting_time = []
+    
+    sim_data = a.NetworkSimulationAnalytics
+
     # Initiate the simulation class with appropriate data arrays.
-    for l in LAMBDA:
+    for lmbd in LAMBDA:
         netsim = ns.NetworkSimulation(
-            interarrival_time=[0] + exp_rand_num_gen(seed=5, rate=l, size=99_999),
+            interarrival_time=[0] + exp_rand_num_gen(seed=5, rate=lmbd, size=99_999),
             service_time=exp_rand_num_gen(seed=13, rate=MU, size=100_000),
         )
         results = netsim.simulate(
             BUFFER=-1,
-            SERVER_NUM=2,
+            SERVER_NUM=k,
         )
 
-        avg_n_packets.append(netsim_data.\
+        simulation_avg_n_packets.append(sim_data.\
             average_number_of_packets_in_system(results['time_spent_in_system'], 
                                                 results['departure_time']))
-        avg_queue_delay.append(netsim_data.\
+        simulation_avg_queue_delay.append(sim_data.\
             average_queuing_delay(results['queue_delay']))
 
-        avg_waiting_time.append(netsim_data.\
+        simulation_avg_waiting_time.append(sim_data.\
             average_time_spent_in_system(results['time_spent_in_system']))
+        
+        th_data = th.TheoreticalEquations(LAMBDA=lmbd, MU=MU, k=k)
+        theoretical_avg_n_packets.append(th_data.L())
+        theoretical_avg_queue_delay.append(th_data.Wq())
+        theoretical_avg_waiting_time.append(th_data.W())
 
     netsim_graph = a.NetworkSimulationGraphs
 
     netsim_graph.average_number_of_packets_in_system(
         arrival_rates=LAMBDA,
-        theoretical_results=[1,1,1,1,1,1],
-        simulation_results=avg_n_packets
+        theoretical_results=theoretical_avg_n_packets,
+        simulation_results=simulation_avg_n_packets
     )
     netsim_graph.average_queuing_delay(
         arrival_rates=LAMBDA,
-        theoretical_results=[1,1,1,1,1,1],
-        simulation_results=avg_queue_delay
+        theoretical_results=theoretical_avg_queue_delay,
+        simulation_results=simulation_avg_queue_delay
     )
     netsim_graph.average_waiting_time_delay(
         arrival_rates=LAMBDA,
-        theoretical_results=[1,1,1,1,1,1],
-        simulation_results=avg_waiting_time
+        theoretical_results=theoretical_avg_waiting_time,
+        simulation_results=simulation_avg_waiting_time
     )
+
+    print(th.TheoreticalEquations(LAMBDA=1, MU=4, k=k).L())
+    print(th.TheoreticalEquations(LAMBDA=3, MU=4, k=k).L())
+    print(th.TheoreticalEquations(LAMBDA=5, MU=4, k=k).L())
+    print(th.TheoreticalEquations(LAMBDA=7, MU=4, k=k).L())
+    print()
+    print(th.TheoreticalEquations(LAMBDA=1, MU=4, k=k).Wq())
+    print(th.TheoreticalEquations(LAMBDA=3, MU=4, k=k).Wq())
+    print(th.TheoreticalEquations(LAMBDA=5, MU=4, k=k).Wq())
+    print(th.TheoreticalEquations(LAMBDA=7, MU=4, k=k).Wq())
+    print()
+    print(th.TheoreticalEquations(LAMBDA=1, MU=4, k=k).W())
+    print(th.TheoreticalEquations(LAMBDA=3, MU=4, k=k).W())
+    print(th.TheoreticalEquations(LAMBDA=5, MU=4, k=k).W())
+    print(th.TheoreticalEquations(LAMBDA=7, MU=4, k=k).W())
+    print()
 
 
 # Execute the program.
--- theoretical.py
+++ theoretical.py
@@ -23,12 +23,50 @@
 #
 
 
-class TheoreticalVerification:
-    def __init__(self, LAMBDA, MU, K):
+from math import e
+from math import factorial
+
+
+class TheoreticalEquations:
+    def __init__(self, LAMBDA=None, MU=None, k=None):
         self.LAMBDA = LAMBDA
         self.MU = MU
-        self.K = K
+        self.k = k
 
-    def results(self):
+    def p(self):
+        return self.LAMBDA / self.MU
 
-        return
+    def P0(self):
+        summation = 0
+        for n in range(self.k):
+            summation += ((self.LAMBDA / self.MU) ** n) / factorial(n)
+        return (summation + (((self.LAMBDA / self.MU) ** self.k) * self.k * self.MU) / \
+            (factorial(self.k) * ((self.k * self.MU) - self.LAMBDA))) ** -1
+
+    def C(self):
+        return (self.k * (self.p() ** self.k) * self.P0()) / \
+            (factorial(self.k) * (self.k - self.p()))
+
+    def Lq(self):
+        if self.k >= 2:
+            return (self.LAMBDA * self.C()) / \
+                ((self.MU * self.k) - self.LAMBDA)
+        else:
+            return self.LAMBDA * self.Wq()
+
+    def Wq(self):
+        if self.k >= 2:
+            return self.Lq() / self.LAMBDA
+        else:
+            if self.p() != 1:
+                return self.p() / (self.MU - self.LAMBDA)
+
+    def W(self):
+        if self.k >= 2:
+            return self.Wq() + (1 / self.MU)
+
+    def L(self):
+        if self.k >= 2:
+            return self.LAMBDA * self.W()
+        else:
+            return (self.LAMBDA / self.MU) + self.Lq()
