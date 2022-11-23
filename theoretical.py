4029d3915324fe9ac94432025d076e79a4f424ae
Noah Da Silva <noah@noahdasilva.com>
2022-11-22 22:52:03 -0500

    Adjusted theoretical equations

--- theoretical.py
+++ theoretical.py
@@ -33,40 +33,87 @@ class TheoreticalEquations:
         self.MU = MU
         self.k = k
 
+        self._p = None
+        self._P0 = None
+        self._C = None
+        self._Lq = None
+        self._Wq = None
+        self._W = None
+        self._L = None
+        
     def p(self):
-        return self.LAMBDA / self.MU
+        self._p = self.LAMBDA / self.MU
+
+        return self._p
 
     def P0(self):
         summation = 0
         for n in range(self.k):
             summation += ((self.LAMBDA / self.MU) ** n) / factorial(n)
-        return (summation + (((self.LAMBDA / self.MU) ** self.k) * self.k * self.MU) / \
-            (factorial(self.k) * ((self.k * self.MU) - self.LAMBDA))) ** -1
+
+        self._P0 = (summation + (((self.LAMBDA / self.MU) ** self.k) * self.k * self.MU) / \
+            (factorial(self.k) * ((self.k * self.MU) - self.LAMBDA))) ** (-1)
+ 
+        return self._P0
 
     def C(self):
-        return (self.k * (self.p() ** self.k) * self.P0()) / \
-            (factorial(self.k) * (self.k - self.p()))
+        if self._p is None:
+            self.p()
+        if self._P0 is None:
+            self.P0()
+
+        self._C = (self.k * (self._p ** self.k) * self._P0) / \
+            (factorial(self.k) * (self.k - self._p))
+
+        return self._C
 
     def Lq(self):
+        if self._C is None:
+            self.C()
+        if self._p is None:
+            self.p()
+
         if self.k >= 2:
-            return (self.LAMBDA * self.C()) / \
+            self._Lq = (self.LAMBDA * self._C) / \
                 ((self.MU * self.k) - self.LAMBDA)
         else:
-            return self.LAMBDA * self.Wq()
+            self._Lq = (self._p ** 2) / (1 - self._p)
+
+        return self._Lq
 
     def Wq(self):
+        if self._Lq is None:
+            self.Lq()
+        if self._p is None:
+            self.p()
+
         if self.k >= 2:
-            return self.Lq() / self.LAMBDA
+            self._Wq = self._Lq / self.LAMBDA
         else:
-            if self.p() != 1:
-                return self.p() / (self.MU - self.LAMBDA)
+            self._Wq = self._p / (self.MU - self.LAMBDA)
+
+        return self._Wq
 
     def W(self):
+        if self._Wq is None:
+            self.Wq()
+
         if self.k >= 2:
-            return self.Wq() + (1 / self.MU)
+            self._W = self._Wq + (1 / self.MU)
+        else:
+            self._W = 1 / (self.MU - self.LAMBDA)
+
+        return self._W
 
     def L(self):
+        if self._W is None:
+            self.W()
+        if self._Lq is None:
+            self.Lq()
+
         if self.k >= 2:
-            return self.LAMBDA * self.W()
+            self._L = self.LAMBDA * self._W
         else:
-            return (self.LAMBDA / self.MU) + self.Lq()
+            self._L = (self.LAMBDA / self.MU) + self._Lq
+
+        return self._L
